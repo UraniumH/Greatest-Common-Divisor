27e05fba81a2749edb29ede14581ac1caa8dd0e5
Debug AHS <34787483+UraniumH@users.noreply.github.com>
2018-01-06 07:40:28 +0000

    Create enumeration.cpp

---
+++ enumeration.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#define llong unsigned long long
+using namespace std;
+int main(){
+	llong a,b;
+	cin>>a>>b;
+	llong temp;
+	for(llong i=min(a,b);i>0;i-=1){
+		if(a%i==0&&b%i==0){
+			temp=i;
+			break;
+		}
+	}
+	cout<<"Greatest common devisor: "<<temp<<endl;
+	cout<<"Smallest common multiplier: "<<(a/temp)*b<<endl;
+	getchar();
+	return 0;
+}
