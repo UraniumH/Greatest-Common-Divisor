cdd0c1adb5c84c571528619bc6fc12575fd42881
Java AHS <34786330+ChlorineX@users.noreply.github.com>
2018-01-06 08:25:20 +0000

    Update compare.cpp

--- compare.cpp
+++ compare.cpp
@@ -2,28 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define llong unsigned long long
 using namespace std;
-llong zzxc(llong fst,llong scd){
+double zzxc(double fst,double scd){
 	if(fst%scd==0){
 		return scd;
 	}else{
 		return zzxc(scd,fst%scd);
 	}
 }
-void recursion(int a,int b){
+void recursion(double a,double b){
 	clock_t begin,end;
 	begin = clock();
-	llong temp=zzxc(a,b);
+	double temp=zzxc(a,b);
 	end = clock();
 	cout<<"GCD calculated by recursion algorithm: "<<temp<<endl;
 	cout<<"SCM calculated by recursion algorithm: "<<(a/temp)*b<<endl;
 	cout<<"Time used: "<<(end-begin)<<endl<<endl;
 }
 
-void enumeration(int a,int b){
+void enumeration(double a,double b){
 	clock_t begin,end;
-	llong temp;
+	double temp;
 	begin = clock();
 	for(llong i=min(a,b);i>0;i-=1){
 		if(a%i==0&&b%i==0){
@@ -37,13 +36,13 @@ void enumeration(int a,int b){
 	cout<<"Time used: "<<(end-begin)<<endl<<endl;
 }
 
-void multiplication(int a,int b){
+void multiplication(double a,double b){
 	clock_t begin,end;
-	llong temp;
+	double temp;
 	begin = clock();
-	llong max=(a>b)?a:b;
-	llong min=(a<b)?a:b;
-	for(llong i=1;;i+=1){
+	double max=(a>b)?a:b;
+	double min=(a<b)?a:b;
+	for(double i=1;;i+=1){
 		if((max*i)%min==0){
 			temp=max*i;
 			break;
@@ -55,7 +54,7 @@ void multiplication(int a,int b){
 	cout<<"Time used: "<<(end-begin)<<endl<<endl;
 }
 
-bool hz(int a,int b){
+bool hz(double a,double b){
 	while(1){
 		int t = a%b;
 		if(t == 0){
@@ -71,13 +70,13 @@ bool hz(int a,int b){
 		return true;
 	}
 }
-void shortdivision(int a,int b){
+void shortdivision(double a,double b){
 	clock_t begin,end;
-	llong temp=1;
+	double temp=1;
 	begin = clock();
-	llong min=(a<b)?a:b;
+	double min=(a<b)?a:b;
 	for(;;){
-		for(llong i=1;i<=min;i+=1){
+		for(double i=1;i<=min;i+=1){
 			if(a%i==0&&b%i==0){
 				temp*=i;
 				a/=i;
@@ -96,10 +95,10 @@ void shortdivision(int a,int b){
 }
 
 int main(int argc,char *argv[]){
-	llong a,b;
+	double a,b;
 	if(argc==2){
-		a=atoi(argv[0]);
-		b=atoi(argv[1]);
+		a=atof(argv[0]);
+		b=atof(argv[1]);
 	}else{
 		cin>>a>>b;
 	}
