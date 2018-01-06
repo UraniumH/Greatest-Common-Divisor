cf2e5f969975e6bbc7eba369a44bb4416a2c071a
Debug AHS <34787483+UraniumH@users.noreply.github.com>
2018-01-06 07:41:00 +0000

    Create multiplication.cpp

---
+++ multiplication.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define llong unsigned long long
+using namespace std;
+int main(){
+	llong a,b;
+	cin>>a>>b;
+	llong temp;
+	llong max=(a>b)?a:b;
+	llong min=(a<b)?a:b;
+	for(llong i=1;;i+=1){
+		if((max*i)%min==0){
+			temp=max*i;
+			break;
+		}
+	}
+	cout<<"Greatest common devisor: "<<a/(temp/b)<<endl;
+	cout<<"Smallest common multiplier: "<<temp<<endl;
+	getchar();
+	return 0;
+}
